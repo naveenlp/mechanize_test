24a20c2f0addf5502f8dbc87ecf5529e0f3e923d
Naveen LP <naveenlp@gmail.com>
2013-05-22 03:25:20 +0800

    have added google and bing results

--- mechanize_test.rb
+++ mechanize_test.rb
@@ -1,17 +1,47 @@
 require 'rubygems'
 require 'mechanize'
+require 'ostruct'
 
 agent = Mechanize.new
-page = agent.get("http://www.google.com/")
-google_form = page.form('f')
+google_form = agent.get("http://www.google.com/").form('f')
 google_form.q = "maldives"
 search_results = agent.submit(google_form, google_form.buttons.first)
 
+@google_results = Array.new()
 search_results.search("li.g").each do |search_list|
 	link = search_list.search("a").first
-	puts link.text
-	puts link.attribute('href').to_s
-	puts "\n"
+	if(link.attribute('href').to_s.include?("/url?q="))
+		link_obj = OpenStruct.new({ "text" => link.text, "url" => link.attribute('href').to_s.gsub('/url?q=','') });
+		@google_results << link_obj
+
+		puts link_obj.text 
+		puts link_obj.url
+		# puts link.text
+		# puts link.attribute('href').to_s
+		# puts "\n"
+	end
+
+	# puts (link.to_s + "\n\n") if !((link.attribute('href').to_s.include?("/url?")) || (link.attribute('href').to_s.include?("/search?")))
+	 # if link.text != ("Cached" || "Similar")
+	# puts search_list.dom_id.to_s
+end
+
+search_results = agent.get('http://www.bing.com/').forms[0].tap{|f| f.q = 'test'}.submit
+
+
+@bing_results = Array.new()
+
+search_results.search("li.sa_wr").each do |search_list|
+	link = search_list.search("a").first
+	link_obj = OpenStruct.new({ "text" => link.text, "url" => link.attribute('href').to_s});
+	@bing_results << link_obj
+
+	puts link_obj.text 
+	puts link_obj.url
+		# puts link.text
+		# puts link.attribute('href').to_s
+		# puts "\n"
+	
 	# puts (link.to_s + "\n\n") if !((link.attribute('href').to_s.include?("/url?")) || (link.attribute('href').to_s.include?("/search?")))
 	 # if link.text != ("Cached" || "Similar")
 	# puts search_list.dom_id.to_s

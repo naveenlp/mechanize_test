d5ac74f7b342476f96814694b4b718de82fb0bc1
Naveen LP <naveenlp@gmail.com>
2013-05-22 04:37:53 +0800

    fully completed the mechanize test program

--- app/workers/form_submitter.rb
+++ app/workers/form_submitter.rb
@@ -6,19 +6,20 @@ class FormSubmitter
 	sidekiq_options retry: false
 
 	def perform(search_data)
-		$redis["saved_search_term"] = search_data.to_s
+		search_string = search_data.to_s
+		$redis["saved_search_term"] = search_string
   		$redis["search_status"] = "started"
   		
   		agent = Mechanize.new
   		google_form = agent.get("http://www.google.com/").form('f')
-  		google_form.q = "maldives"
+  		google_form.q = search_string
   		search_results = agent.submit(google_form, google_form.buttons.first)
 
   		@google_results = Array.new()
   		search_results.search("li.g").each do |search_list|
   			link = search_list.search("a").first
   			if(link.attribute('href').to_s.include?("/url?q="))
-  				link_obj = { "text" => link.text, "url" => link.attribute('href').to_s.gsub('/url?q=','') };
+  				link_obj = { "text" => link.text, "url" => link.attribute('href').to_s.gsub('/url?q=','').gsub(/&sa=.*/, '') };
   				@google_results << link_obj
 
 				Rails.logger.debug link_obj["text"] 
@@ -28,7 +29,7 @@ class FormSubmitter
 
 		end
 
-		search_results = agent.get('http://www.bing.com/').forms[0].tap{|f| f.q = 'test'}.submit
+		search_results = agent.get('http://www.bing.com/').forms[0].tap{|f| f.q = search_string}.submit
 		@bing_results = Array.new()
 
 		search_results.search("li.sa_wr").each do |search_list|
